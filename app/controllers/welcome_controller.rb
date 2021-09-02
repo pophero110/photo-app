fcf4be038a63c977786306a3287ba61444b1ffa7
pophero110 <38047622+pophero110@users.noreply.github.com>
2021-09-01 19:09:06 -0500

    debugging heroku env

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,7 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
+    p ENV["SENDGRID_PASSWORD"]
+    p ENV["SENDGRID_USERNAME"]
   end
 end
